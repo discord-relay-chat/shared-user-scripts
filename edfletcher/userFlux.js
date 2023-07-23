c448cbfcd1ad08b88b1db52eab7089161102d16e
Ed Fletcher <edfletcher@discordrc.com>
2023-07-23 18:39:36 +0000

    add edfletcher/userFlux.js

---
+++ edfletcher/userFlux.js
@@ -0,0 +1,46 @@
+// Requires constants.MESSAGE_COUNT be set. For every MESSAGE_COUNT messages
+// seen, user flux stats will be emitted to the script/bot channel
+
+/* globals DRCUserScript, sendToBotChan, util */
+
+const { eventName, constants } = DRCUserScript;
+const events = ['irc:quit', 'irc:join', 'irc:part', 'irc:kick', 'irc:ban'];
+
+if (events.includes(eventName)) {
+    const state = await DRCUserScript.state.get();
+
+    if (!state.events) {
+        state.events = {};
+    }
+
+    if (!state.last) {
+        state.last = events.reduce((a, ev) => ({ [ev]: 0, ...a }), {});
+    }
+
+    if (!state.events[eventName]) {
+        state.events[eventName] = 0;
+    }
+
+    state.events[eventName]++;
+    const totals = Object.values(state.events).reduce((a, x) => a + x);
+    const limit = constants.MESSAGE_COUNT || 1000;
+
+    if (!(totals % limit)) {
+        const dur = util.fmtDuration(state.lastAnnounce);
+        sendToBotChan(`${limit} user-flux events over ${dur} (${totals} total):\n` +
+            Object.entries(state.events).map(([name, count]) => {
+                const d = count - state.last[name];
+                return `* \`${name}\` (${count} / ${d > 0 ? '+' : ''}${d})`;
+            }).join('\n'));
+        state.last = { ...state.events };
+        state.events = null;
+        state.lastAnnounce = new Date();
+    }
+
+    if (!state?.lastAnnounce) {
+        state.lastAnnounce = new Date();
+    }
+    
+    state.lastTotal = totals;
+    await DRCUserScript.state.set(state);
+}
